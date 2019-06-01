78adadca017ce93540a286a20187d11784778e35
schinma <marie.schindler@epitech.eu>
2019-06-01 22:47:50 +0900

    Start ennemy drops

---
+++ Assets/Scripts/Orb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehaviour
+{
+    public int health = 20;
+    public float timer = 20;
+    public float chance = 50;
+
+    private float timerCount;
+
+    private void Awake()
+    {
+        timerCount = timer;
+    }
+
+    private void Update()
+    {
+        timerCount -= Time.deltaTime;
+
+        if(timerCount < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Damage")
+        {
+            Debug.Log("projectile trigger");
+            Destroy(this.gameObject);
+        }
+
+    }
+}
