73f5d6a555a3404e5d7cdb687875ef8456e75d3e
Timothée Puentes <timothee.puentes@epitech.eu>
2019-06-11 19:39:19 +0900

    Solve last room y pos start

---
+++ Assets/Scripts/Enemy/SimpleHittable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public class SimpleHittable : Hittable
+    {
+        public ParticleSystem HitParticleSystem;
+        public AudioSource AudioSource;
+        
+        public override void Hit(float damage, Vector3 position)
+        {
+            if (AudioSource != null)
+            {
+                AudioSource.transform.position = position;
+                AudioSource.Play();
+            }
+            if (HitParticleSystem != null)
+            {
+                HitParticleSystem.transform.position = position;
+                HitParticleSystem.Play();
+            }
+        }
+    }
+}
